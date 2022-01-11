8e9d14fa7a90b3ccfc9643c386a0ab20f6be235a
Sameh Botros <samehhabib99@gmail.com>
2022-01-10 20:11:13 -0700

    confirmation component

--- src/app/shop/confirmation-checkout/confirmation-checkout.component.ts
+++ src/app/shop/confirmation-checkout/confirmation-checkout.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { PurchasedItem } from "../../models/purchasedItem.model";
+import { ProductAndDiscount } from 'src/app/models/product.model';
+import { PurchasedItemProduct } from 'src/app/models/purchased-item.model';
+import { PurchasedItemService } from 'src/app/services/purchased-item.service';
 
 @Component({
   selector: 'app-confirmation-checkout',
@@ -9,19 +11,28 @@ import { PurchasedItem } from "../../models/purchasedItem.model";
 })
 export class ConfirmationCheckoutComponent implements OnInit {
 
-  purchasedItems: PurchasedItem[] = [];
+  purchasedItemProduct: PurchasedItemProduct[] = [];
   errorMsg: string = "";
-  constructor(private activatedRoute: ActivatedRoute) { }
+  transId: any = 0;
+  constructor(private activatedRoute: ActivatedRoute, private purchasedItemService: PurchasedItemService) { }
 
   ngOnInit(): void {
-    let transId = this.activatedRoute.snapshot.paramMap.get("sentTransaction");
-    console.log("helooo")
-
-    //   this.purchasedItems.purchasedItemservice(transId).subscribe((response) => {
-    //     this.purchasedItems = response
-    //   }, (error) => {
-    //     this.errorMsg = 'There was some internal error! Please try again later!';
-    //   })
-
+    this.transId = this.activatedRoute.snapshot.paramMap.get("sentTransaction");
+    console.log("my product " + this.transId);
+    this.loadItems();
   }
+
+
+  loadItems() {
+    console.log("start load");
+    this.purchasedItemService.getPurchasedItemsByTransaction(this.transId).subscribe((response) => {
+      this.purchasedItemProduct = response;
+      console.log("tester test");
+      console.log(response)
+    }, error => {
+      this.errorMsg = 'There was some internal error! Please try again later!';
+      console.log(error);
+    });
+  }
+
 }
