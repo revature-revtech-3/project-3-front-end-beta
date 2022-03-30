4e5e660c35835842d68701191d8d584de8e0e752
TylerSBoston <98355458+TylerSBoston@users.noreply.github.com>
2022-03-30 16:01:49 -0400

    commit test

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -11,4 +11,13 @@ export class User {
     contact: string = "";
     imageUrl: string = "";
     userRemoved: boolean = false;
+    // add after current user problems solved
+    // userSettings: UserSettings = null;
+}
+
+export class UserSettings {
+    user_settings_id: number = 0;
+    user_id: number = 0;
+    css_mode: string = "";
+    email_notifications: boolean = true;
 }
