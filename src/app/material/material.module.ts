1d6979650d06e28cd12b073858e24b117d88d153
rebeccaAtRevature <98355913+rebeccaAtRevature@users.noreply.github.com>
2022-04-02 15:25:40 -0400

    added logo and toolbar - unfinished

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+
+const MaterialComponents = [
+  MatButtonModule,
+  MatCardModule,
+  MatToolbarModule,
+  MatMenuModule,
+  MatIconModule,
+  MatTabsModule
+]
+
+@NgModule({
+  declarations: [],
+  imports: [ MaterialComponents ],
+  exports: [ MaterialComponents ]
+})
+export class MaterialModule { }
