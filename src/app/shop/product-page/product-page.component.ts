838f2388718357c75c9b875206fb744e423286f4
Kouame <kouamedjah@example.com>
2022-05-09 13:26:02 -0500

    Updated Product Page

--- src/app/shop/product-page/product-page.component.ts
+++ src/app/shop/product-page/product-page.component.ts
@@ -74,6 +74,12 @@ export class ProductPageComponent implements OnInit {
   };
   Xbox: number[] = [1,7];
   Cam: number[] = [8,9,10,11,12];
+  Apple: number[] = [2,3,6,16];
+  Samsung: number[] = [14,15];
+  Headphones: number[] =[5,13];
+  Dell: number[] = [4,17]
+  Group: string[] = ["Xbox","Cam","Apple","Samsung","Headphones","Dell"];
+  Families: any[] = [this.Xbox,this.Cam,this.Apple,this.Samsung,this.Headphones,this.Dell];
   
   sproduct: boolean|any;
   sproduct_name: String |any;
@@ -112,20 +118,24 @@ this.loadSecondaryProduct(this.productId);
   }
 
 loadSecondaryProduct(p_id:number){
-if(this.sproduct = this.Xbox.includes(p_id)){
-  alert("it is in here" + p_id)
-this.sproduct_name = "Xbox";
-this.productService.getByNameProductsService(this.sproduct_name).subscribe({
-  next: (response) => {
-    this.productbyname = response;
-    //this.productLoaded = true;
-    //console.log(this.sproduct.productName);
-  },
-  error: (error) => { },
-})
-} else{
-  alert("It is not in here"+ p_id);
-}
+  for(let i= 0; i< this.Families.length; i++){
+
+    if(this.sproduct = this.Families[i].includes(p_id)){
+      alert("it is in here" + p_id)
+    this.sproduct_name = this.Group[i];
+    this.productService.getByNameProductsService(this.sproduct_name).subscribe({
+      next: (response) => {
+        this.productbyname = response;
+        //this.productLoaded = true;
+        //console.log(this.sproduct.productName);
+      },
+      error: (error) => { },
+    })
+    } else{
+      //alert("It is not in here"+ p_id);
+    }
+  }
+
   
 
 }
