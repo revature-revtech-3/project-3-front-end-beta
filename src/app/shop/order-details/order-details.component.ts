16c057be01980f6017f3a4e8ae1f77bf1c8fed51
kiieer <christliebkiersten@gmail.com>
2022-05-16 12:54:48 -0400

    added a few features to the order-details component.

--- src/app/shop/order-details/order-details.component.ts
+++ src/app/shop/order-details/order-details.component.ts
@@ -10,6 +10,10 @@ import { PurchasedItemService } from 'src/app/services/purchased-item.service';
 })
 export class OrderDetailsComponent implements OnInit {
   cId: any = 0;
+  tId: any = 0;
+  total: any = 0;
+  itemsAmt: any = 0;
+  datepurchased: any = new Date();
   purchasedItemProduct: PurchasedItemProduct[] = [];
   errorMsg: string = "";
   
@@ -23,6 +27,16 @@ export class OrderDetailsComponent implements OnInit {
   loadItems(cId: any) {
     this.purchasedItemService.getPurchasedItemsByOrder(this.cId).subscribe((response) => {
       this.purchasedItemProduct = response;
+      this.tId = this.purchasedItemProduct[0].transactionId;
+      this.datepurchased = this.purchasedItemProduct[0].purchaseDate;
+      for (let i = 0; i < this.purchasedItemProduct.length; i++) {
+        let amt = this.purchasedItemProduct[i].itemQty;
+        this.itemsAmt = this.itemsAmt + amt;
+      }
+      for (let i = 0; i < this.purchasedItemProduct.length; i++) {
+        let amt = this.purchasedItemProduct[i].purchaseCost;
+        this.total = this.total + amt;
+      }
       console.log(this.purchasedItemProduct)
     }, error => {
       this.errorMsg = 'There was some internal error! Please try again later!';
